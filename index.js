6a417753e2f3d82553883a4f502684f9bb85be6b
abdul-dulal <dulal.dpi.387364@gmail.com>
2022-09-08 21:34:37 +0600

    error

--- index.js
+++ index.js
@@ -10,24 +10,6 @@ app.use(express.json());
 app.use(cors());
 const port = process.env.PORT || 3000;
 
-mongoose
-  .connect(process.env.MONGOOSE_URI)
-  .then(() => {
-    console.log("database connect");
-  })
-  .catch((err) => {
-    err.message;
-  });
-
-// Product
-app.use("/product", handleProduct);
-
-// Review
-app.use("/review", handleReview);
-
-//order
-app.use("/order", hanldeOrder);
-
 app.get("/", (req, res) => {
   res.send("hello from Tools-Shop");
 });
